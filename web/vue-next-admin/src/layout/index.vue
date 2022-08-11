d9a2512698e34c3b397bcc99865e288a07bee8db
dbsgw <admin@dbsgw.cn>
2022-08-11 23:05:55 +0800

    总结

--- web/vue-next-admin/src/layout/index.vue
+++ web/vue-next-admin/src/layout/index.vue
@@ -1,36 +1,58 @@
 <template>
+<!--  切换组件的-->
 	<component :is="themeConfig.layout" />
 </template>
 
 <script lang="ts">
+// onBeforeMount,  DOM即将挂载生命周期
+// onUnmounted,   销毁完毕完毕生命周期
+// getCurrentInstance,   获取实例的的 类型 vue2的 this
+// defineComponent,  默认加载 setup的
+// defineAsyncComponent  异步加载组件的
 import { onBeforeMount, onUnmounted, getCurrentInstance, defineComponent, defineAsyncComponent } from 'vue';
+// 状态管理工具， 导出解构赋值 响应式的
 import { storeToRefs } from 'pinia';
+// 模板的状态管理
 import { useThemeConfig } from '/@/stores/themeConfig';
+// localStorage缓存
 import { Local } from '/@/utils/storage';
 
 export default defineComponent({
 	name: 'layout',
+  // 注册组件
 	components: {
+    // 导入异步组件
 		defaults: defineAsyncComponent(() => import('/@/layout/main/defaults.vue')),
 		classic: defineAsyncComponent(() => import('/@/layout/main/classic.vue')),
 		transverse: defineAsyncComponent(() => import('/@/layout/main/transverse.vue')),
 		columns: defineAsyncComponent(() => import('/@/layout/main/columns.vue')),
 	},
 	setup() {
+    // 获取 当前组件实例，
 		const { proxy } = <any>getCurrentInstance();
+    // 模板状态 初始化
 		const storesThemeConfig = useThemeConfig();
+    // 模板状态 解构响应式 出来
 		const { themeConfig } = storeToRefs(storesThemeConfig);
 		// 窗口大小改变时(适配移动端)
 		const onLayoutResize = () => {
+      // 缓存有没有 oldLayout 模板，没有就 存一个，从  状态里面获取的
 			if (!Local.get('oldLayout')) Local.set('oldLayout', themeConfig.value.layout);
+      // 当前body 的宽度
 			const clientWidth = document.body.clientWidth;
+
 			if (clientWidth < 1000) {
+        // 宽度小于 1000
+        // 模板的 侧边栏 是否开启菜单水平折叠效果
 				themeConfig.value.isCollapse = false;
+        // 跨组件通讯 传入默认 模板 和当前 宽度
 				proxy.mittBus.emit('layoutMobileResize', {
 					layout: 'defaults',
 					clientWidth,
 				});
 			} else {
+        // 宽度大于1000
+        // 跨组件通讯 传入 缓存模板/没有就传入 状态里面的初始化模板，和宽度
 				proxy.mittBus.emit('layoutMobileResize', {
 					layout: Local.get('oldLayout') ? Local.get('oldLayout') : themeConfig.value.layout,
 					clientWidth,
@@ -39,11 +61,14 @@ export default defineComponent({
 		};
 		// 页面加载前
 		onBeforeMount(() => {
+      // 初始化了 窗口大小
 			onLayoutResize();
+      // 注册监听 页面 响应大小时间  触发 改变窗口大小事件
 			window.addEventListener('resize', onLayoutResize);
 		});
 		// 页面卸载时
 		onUnmounted(() => {
+      // 页面 关闭时候 卸载 监听事件
 			window.removeEventListener('resize', onLayoutResize);
 		});
 		return {
--- web/vue-next-admin/src/layout/main/defaults.vue
+++ web/vue-next-admin/src/layout/main/defaults.vue
@@ -1,34 +1,58 @@
 <template>
+<!--  可视区 滑动 滚动区域-->
 	<el-container class="layout-container">
+<!--    测边栏-->
 		<Aside />
+    <!--  可视区 滑动 滚动区域-->
 		<el-container class="flex-center" :class="{ 'layout-backtop': !isFixedHeader }">
+<!--      顶部-->
 			<Header v-if="isFixedHeader" />
+      <!--  可视区 滑动 滚动区域-->
 			<el-scrollbar ref="layoutDefaultsScrollbarRef" :class="{ 'layout-backtop': isFixedHeader }">
+<!--        内部顶部-->
 				<Header v-if="!isFixedHeader" />
+<!--        主内容区域-->
 				<Main />
 			</el-scrollbar>
 		</el-container>
+<!--    回到顶部-->
 		<el-backtop target=".layout-backtop .el-scrollbar__wrap"></el-backtop>
 	</el-container>
 </template>
 
 <script lang="ts">
+//computed,   计算属性
+//getCurrentInstance,  当前组件实例
+//watch,  监听
+//defineComponent  默认导出 setup的
 import { computed, getCurrentInstance, watch, defineComponent } from 'vue';
+// 接收路由参数的
 import { useRoute } from 'vue-router';
+// 状态管理 引入响应式的
 import { storeToRefs } from 'pinia';
+// 模板组件
 import { useThemeConfig } from '/@/stores/themeConfig';
+// 测边栏
 import Aside from '/@/layout/component/aside.vue';
+// 顶部
 import Header from '/@/layout/component/header.vue';
+// 主内容
 import Main from '/@/layout/component/main.vue';
 
 export default defineComponent({
 	name: 'layoutDefaults',
+  // 注册组件
 	components: { Aside, Header, Main },
 	setup() {
+    // 获取当前组件实例
 		const { proxy } = <any>getCurrentInstance();
+    // 获取当前路由实例
 		const route = useRoute();
+    // 初始化 状态管理
 		const storesThemeConfig = useThemeConfig();
+    // 响应式状态管理
 		const { themeConfig } = storeToRefs(storesThemeConfig);
+    // 计算属性 计算 在哪里显示 顶部栏
 		const isFixedHeader = computed(() => {
 			return themeConfig.value.isFixedHeader;
 		});
@@ -36,6 +60,7 @@ export default defineComponent({
 		watch(
 			() => route.path,
 			() => {
+        // 路由变化  手动把  可视区 滑动 滚动区域  跳转到 顶部
 				proxy.$refs.layoutDefaultsScrollbarRef.wrap$.scrollTop = 0;
 			}
 		);
